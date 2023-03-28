a2f218a2781d28c2f6fc12e0476975818eb3864e
Fabricio Sousa <fabricio_370@hotmail.com>
2023-03-28 23:03:07 +0100

    solved lab

--- js/index.js
+++ js/index.js
@@ -6,21 +6,19 @@ function updateSubtotal(product) {
   const price = product.querySelector('.price span');
   const quantity = product.querySelector('.quantity input');
 
-  const priceValue = parseFloat(price.innerText)
-  const quantityValue = quantity.valueAsNumber
+  const priceValue = parseFloat(price.innerText);
+  const quantityValue = quantity.valueAsNumber;
 
-  const subtotal = priceValue * quantityValue
-  
-  let subtotalHtml = product.querySelector(".subtotal span")
+  const subtotal = priceValue * quantityValue;
 
-  subtotalHtml.innerText = subtotal
+  let subtotalHtml = product.querySelector('.subtotal span');
 
-  return subtotal
- 
+  subtotalHtml.innerText = subtotal;
+
+  return subtotal;
 }
 
 function calculateAll() {
-  
   let products = document.getElementsByClassName('product');
 
   let total = 0;
@@ -28,7 +26,7 @@ function calculateAll() {
   // end of test
 
   // ITERATION 2
-  for( let i = 0; i < products.length; i++) {
+  for (let i = 0; i < products.length; i++) {
     total += updateSubtotal(products[i]);
   }
 
@@ -37,11 +35,10 @@ function calculateAll() {
 }
 
 // ITERATION 4
-
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  const td = target.parentNode; 
+  const td = target.parentNode;
   const tr = td.parentNode; // the row to be removed
   tr.parentNode.removeChild(tr);
 }
@@ -49,14 +46,18 @@ function removeProduct(event) {
 // ITERATION 5
 
 function createProduct() {
-  const tbodyRef = document.getElementById('cart').getElementsByTagName('tbody')[0];
-  const inputs = document.getElementsByTagName('tfoot')[0].getElementsByTagName('input');
+  const tbodyRef = document
+    .getElementById('cart')
+    .getElementsByTagName('tbody')[0];
+  const inputs = document
+    .getElementsByTagName('tfoot')[0]
+    .getElementsByTagName('input');
   console.log(inputs);
   const row = tbodyRef.insertRow();
-  row.className = "product";
+  row.className = 'product';
   row.innerHTML = `<td class="name"><span>${inputs[0].value}</span></td><td class="price">$<span>${inputs[1].value}</span></td><td class="quantity"><input type="number" value="0" min="0" placeholder="Quantity" /></td><td class="subtotal">$<span>0</span></td><td class="action"><button class="btn btn-remove">Remove</button></td>`;
   const removeBtns = document.getElementsByClassName('btn-remove');
-  for( let i = 0; i < removeBtns.length; i++) {
+  for (let i = 0; i < removeBtns.length; i++) {
     removeBtns[i].addEventListener('click', removeProduct);
   }
 }
@@ -66,7 +67,7 @@ window.addEventListener('load', () => {
   calculatePricesBtn.addEventListener('click', calculateAll);
 
   const removeBtns = document.getElementsByClassName('btn-remove');
-  for( let i = 0; i < removeBtns.length; i++) {
+  for (let i = 0; i < removeBtns.length; i++) {
     removeBtns[i].addEventListener('click', removeProduct);
   }
 
